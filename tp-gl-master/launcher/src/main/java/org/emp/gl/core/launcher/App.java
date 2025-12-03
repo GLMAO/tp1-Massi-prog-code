10930f43e09714e3310395783bb28801fbe9fdd1
Massi-prog-code <harveysmithforever1998@gmail.com>
2025-12-03 11:47:12 +0100

    add App.java

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -1,10 +1,58 @@
+
 package org.emp.gl.core.launcher;
 
+import org.emp.gl.clients.Horloge;
+import org.emp.gl.clients.CompteARebours;
+import org.emp.gl.timer.service.TimerService;
+import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
+import org.emp.gl.lookup.Lookup;
 import java.util.Random;
 
-import org.emp.gl.clients.CompteARebours;
-import org.emp.gl.clients.Horloge ;
+public class App {
 
+    public static void main(String[] args) {
+        testAvecLookup();
+    }
+
+    private static void testAvecLookup() {
+        System.out.println("=== Test avec Lookup (Annuaire) ===\n");
+        
+        // 1. Récupérer l'instance du Lookup (Singleton)
+        Lookup lookup = Lookup.getInstance();
+        
+        // 2. Créer le TimerService
+        TimerService timerService = new DummyTimeServiceImpl();
+        
+        // 3. Enregistrer le service dans le Lookup
+        lookup.subscribeService("TimerService", timerService);
+        
+        // 4. Afficher les services enregistrés
+        lookup.printServices();
+        
+        System.out.println();
+        
+        // 5. Créer des Horloges (sans passer le TimerService !)
+        new Horloge("Horloge-Paris");
+        new Horloge("Horloge-Londres");
+        new Horloge("Horloge-Tokyo");
+        
+        System.out.println();
+        
+        // 6. Créer des CompteARebours
+        Random random = new Random();
+        for (int i = 1; i <= 5; i++) {
+            int valeur = 5 + random.nextInt(6);
+            new CompteARebours("Compte-" + i, valeur);
+        }
+        
+        // Laisser tourner
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
 /**
  * Hello world!
  *
@@ -24,7 +72,7 @@ public class App {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-}*/
+}
 
 import org.emp.gl.timer.service.TimerService;
 import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
@@ -53,7 +101,7 @@ public class App {
 }
 */
 
-
+/* 
 
 import org.emp.gl.clients.HorlogeGUI;
 
@@ -74,4 +122,5 @@ public class App {
         // Optionnel : ajouter aussi une horloge console
         Horloge horlogeConsole = new Horloge("Console", timerService);
     }
-}
+}*/
+
