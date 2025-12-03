836bff3672eb40ddcab9431e5f9e2cc14a35b319
Massi-prog-code <harveysmithforever1998@gmail.com>
2025-12-03 11:42:43 +0100

    change the class CompteARebours

--- tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/CompteARebours.java
+++ tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/CompteARebours.java
@@ -2,6 +2,7 @@ package org.emp.gl.clients;
 
 import org.emp.gl.timer.service.TimerChangeListener;
 import org.emp.gl.timer.service.TimerService;
+import org.emp.gl.lookup.Lookup;
 import java.beans.PropertyChangeEvent;
 
 public class CompteARebours implements TimerChangeListener {
@@ -10,25 +11,31 @@ public class CompteARebours implements TimerChangeListener {
     private int compteur;
     private TimerService timerService;
     
-    public CompteARebours(String name, int valeurInitiale, TimerService timerService) {
+    // NOUVEAU CONSTRUCTEUR
+    public CompteARebours(String name, int valeurInitiale) {
         this.name = name;
         this.compteur = valeurInitiale;
-        this.timerService = timerService;
         
-        // S'inscrire comme observateur
-        timerService.addTimeChangeListener(this);
+        // Récupérer le TimerService depuis le Lookup
+        Lookup lookup = Lookup.getInstance();
+        Object service = lookup.getService("TimerService");
         
-        System.out.println("CompteARebours " + name + " initialisé à " + compteur);
+        if (service instanceof TimerService) {
+            this.timerService = (TimerService) service;
+            timerService.addTimeChangeListener(this);
+            
+            System.out.println("CompteARebours " + name + " initialisé à " + compteur);
+        } else {
+            throw new RuntimeException("TimerService non trouvé !");
+        }
     }
     
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        // Réagir uniquement aux changements de secondes
         if (SECONDE_PROP.equals(evt.getPropertyName())) {
             compteur--;
             System.out.println(name + " : " + compteur);
             
-            // Se désinscrire quand le compte arrive à 0
             if (compteur <= 0) {
                 System.out.println(name + " terminé ! Désinscription...");
                 timerService.removeTimeChangeListener(this);
